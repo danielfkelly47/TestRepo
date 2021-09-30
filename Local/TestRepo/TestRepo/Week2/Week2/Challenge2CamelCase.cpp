84d74a9006bfb058fb01e456f4a01604cadf8ff7
danielfkelly47 <danielfkelly47@gmail.com>
2021-09-30 15:36:27 +0100

    Camel Case

---
+++ Local/TestRepo/TestRepo/Week2/Week2/Challenge2CamelCase.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main8() {
+	string s1, saveChangesInTheEditor;
+	std::cin >> saveChangesInTheEditor;
+
+	
+}
--- Local/TestRepo/TestRepo/Week2/Week2/StringWarmUp.cpp
+++ Local/TestRepo/TestRepo/Week2/Week2/StringWarmUp.cpp
@@ -16,4 +16,22 @@ int main7() {
 		std: cout << "I pitty the fool";
 		}
 		return 0;
+
+		// Task 2 Comparing strings part II
+		s1 = "HELLO";
+		s2 = "Hello";
+
+		std::cout << s1 << s2 << endl;
+		s1.swap (s2);
+
+		//Task 3 Coded Messages
+
+		if ( s1 == s2)
+		{
+			std::cout << "Holiday or Bwlize";
+		}
+		if (s1== s2) {
+			std::cout << "Yes Shoot them";
+		}
+
 }
