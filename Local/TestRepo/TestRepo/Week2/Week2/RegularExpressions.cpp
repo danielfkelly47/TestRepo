7009c564a6062faf9c29e09bbdac44d589594540
danielfkelly47 <danielfkelly47@gmail.com>
2021-09-30 15:19:54 +0100

    String Warm Up

--- Local/TestRepo/TestRepo/Week2/Week2/RegularExpressions.cpp
+++ Local/TestRepo/TestRepo/Week2/Week2/RegularExpressions.cpp
@@ -7,8 +7,16 @@
 #include <regex>
 
 using namespace std;
+void printMatches(std::string str, std::regex reg ) {
+	std::smatch matches;
+	std::cout << std::boolalpha;
+	while (std::regex_search(str, matches, reg)) {
+		std::cout << "There is a match" << matches.ready ();
+	}
 
-int main() {
+}
+
+int main6() {
 	std::string str = "The Ape was at the Apex";
 	std::smatch matches;
 	std::regex reg("");
---
+++ Local/TestRepo/TestRepo/Week2/Week2/StringWarmUp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main7() {
+	
+		string s1;
+		string s2;
+
+		if (s1 == s2) {
+			std::cout << "This please Chuck Norris";
+		}
+		else
+		{
+		std: cout << "I pitty the fool";
+		}
+		return 0;
+}
