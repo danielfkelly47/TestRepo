d32da5686c256eed4e146072c3bda308a57bd692
danielfkelly47 <danielfkelly47@gmail.com>
2021-09-30 13:36:37 +0100

    For Loops

---
+++ Local/TestRepo/TestRepo/Week2/Week2/StringClassAndStringFunctions.cpp
@@ -0,0 +1,30 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+int main() {
+	string bucky;
+	cin >> bucky;
+	cout << "The String i entered is:" << bucky << endl;
+
+	string x;
+	getline(cin, x);
+	cout << x << endl;
+
+	string s1("Hampster");
+	string s2;
+	string s3;
+
+	s2 = s1;
+	s3.assign(s1);
+
+	cout << s1 << s2 << s3 << endl;
+
+	s1 = "omgwtfbbq";
+	cout << s1.at(3) << endl;
+
+	for (int x =0; x<s1.length();x++)
+	{
+		cout << s1.at(x);
+	}
+}
---
+++ Local/TestRepo/TestRepo/Week2/Week2/forLoops.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+using namespace std;
+int main() {
+	for (int x =1; x < 10; x++)
+	{
+		std::cout << x << endl;
+	}
+
+	for (int x = 5; x < 50; x+=5)
+	{
+		std::cout << x << endl;
+	}
+}
