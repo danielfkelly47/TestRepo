352a775d752f7ae4f08e3a56fe90d0fd3c54d151
danielfkelly47 <danielfkelly47@gmail.com>
2021-10-04 17:12:54 +0100

    String Warm Up

---
+++ Local/TestRepo/Week2Challenges/StingWarmUp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using name space std;
+int main() {
+	string s1 s2;
+
+	if (s1 == s2)
+	{
+		std::cout << "This Pleases Chuck Norris" << endl;
+	}
+	else
+	{
+		std:: << "I Pitty the Fool" << endl;
+	}
+
+	//Comparing Strings Part 2
+	string one("APPLES");
+	string two("apples");
+
+	std::cout << one << two << endl;
+
+	one.swap(two);
+	std::cout << one << two << endl;
+
+}
