9bef036d010789658de8d78ed8aefd7325205b4a
danielfkelly47 <danielfkelly47@gmail.com>
2021-10-11 15:25:24 +0100

    Challenge 2

--- Local/TestRepo/TestRepo/Week2/Week2/Challenge2CamelCase.cpp
+++ Local/TestRepo/TestRepo/Week2/Week2/Challenge2CamelCase.cpp
@@ -2,13 +2,33 @@
 #include <string>
 
 using namespace std;
+int checkUpperCase(char c);
 
 int main8() {
-	string s1, s2;
-	
-	s1 = s2;
-		s1 = "hello";
+	string s, s1, s2
+		int numWords;
+	std::cout << "Enter a Came case here";
+	std::cin >> s;
+
+	for (int i = 0; i <= s.length(); i++)
+	{
+		if (:: isupper(s[i]))
+		{
+			numWords++;
+		}
+	}
+	std::cout << "The number of words is" << numWords << endl;
+	return 0;
+
 		
 	
 	
+}
+int checkUpperChar(char c) {
+	for (int i = 0; i < 27; i++)
+	{
+		if (c ==char('A' + i)) {
+			return 1;
+		}
+	}
 }
